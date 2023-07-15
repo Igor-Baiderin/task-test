75170ad3c86dc415b80840a3455082218adedd95
Игорь Байдерин <ibaid@mail.ru>
2023-07-15 15:15:17 +0500

    init project

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import './bootstrap';
+
+// import {createApp} from "vue";
+
+import {createApp} from "vue/dist/vue.esm-bundler";
+import TestComponent from "@/TestComponent.vue";
+
+const app = createApp({
+    components: {
+        TestComponent,
+    }
+})
+
+app.mount('#app')
+
